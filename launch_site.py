bbed767d1496ae4a831bc503ead3f3cbe7e2185d
Salil Nadkarni <salnad02@gmail.com>
2020-09-12 23:35:35 -0400

    Fucking working

--- launch_site.py
+++ launch_site.py
@@ -5,6 +5,7 @@
 from flask import Flask, render_template, url_for, redirect, request
 from reader import DatabaseHelper
 from functools import reduce
+import random
 
 app = Flask(__name__)
 helper = DatabaseHelper()
@@ -14,18 +15,27 @@ def index():
     if (request.method == 'POST'):
         data = str(request.form["search"])
         print("Search query: ", data)
-        values = getPoliticalSentiment(data)
+        query_result = getPoliticalSentiment(data)
         #TODO: change jinja templating after search goes through or reroute to new template
 
-        return render_template("index.html", query_made=True, previous_query=data)
+        return render_template("index.html", query_made=True, previous_query=data, query_sentiment=query_result['sentiment'], query_example=query_result['example'])
 
     elif (request.method == 'GET'):
         return render_template("index.html", query_made=False, previous_query="")
 
 
 def getPoliticalSentiment(name):
-  sentimentScores = helper.find_by_name(name)
-  sentimentScores = [score['sentiment'] for score in sentimentScores]
+  sentimentBlobs = helper.find_by_name(name)
+  random.shuffle(sentimentBlobs)
+  sentimentScores = [score['sentiment'] for score in sentimentBlobs]
   averageSentiment = reduce(lambda x, y: x + y, sentimentScores) / len(sentimentScores)
-  print(averageSentiment)
-  return averageSentiment
+  example = ""
+  for blob in sentimentBlobs:
+    if abs(blob['sentiment'] - averageSentiment) < 0.1:
+      example = blob['text']
+      break
+  result = {}
+  result['sentiment'] = averageSentiment
+  result['example'] = example
+  print(result)
+  return result
